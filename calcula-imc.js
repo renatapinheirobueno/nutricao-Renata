e1b2071b8dcf46dfe2b202a681e90ced8cce5195
Renata Pinheiro <133382910+renatapinheirobueno@users.noreply.github.com>
2024-10-25 11:42:06 -0300

    Create calcula-imc.js

---
+++ calcula-imc.js
@@ -0,0 +1,44 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Renata Nutrição";
+
+var pacientes = document.querySelectorAll('.paciente');
+
+for (var i = 0; i < 5; i++){
+  console.log(pacientes[i]);
+
+  var tdPeso = pacientes[i].querySelector('.info-peso');
+  var peso = tdPeso.textContent;
+
+  var tdAltura = pacientes[i].querySelector('.info-altura');
+  var altura = tdAltura.textContent;
+
+  var tdIMC = pacientes[i].querySelector('.info-imc');
+
+  var pesoEnValido = true;
+  var alturaEnValida = true;
+
+  if(peso <= 0 || peso >= 600){
+    tdIMC.textContent = "Peso inválido ";
+    pesoEnValido = false;
+    pacientes[i].classList.add('dados-invalidos');
+  }
+
+  if(altura <= 0 || altura > 2.80){
+    tdIMC.textContent = "Altura inválido ";
+    alturaEnValida = false
+    pacientes[i].classList.add('dados-invalidos');
+  }
+
+  if (pesoEnValido && alturaEnValida){
+    var imc = calculaImc(peso, altura);
+    tdIMC.textContent = imc;
+  }
+}
+
+function calculaImc(peso, altura) {
+  var imc = 0;
+  
+  imc = peso / (altura*altura);
+  
+  return imc.toFixed(2);
+     }
